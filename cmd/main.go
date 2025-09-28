0f309c3e3a31c84a9cd42bf83c7432df3debf6d6
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-09-28 19:40:46 -0300

    ⚡️ Adding my first commands for my cli
    so I can use pretty fast comamnds and options for my dev

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gsn-dev-tools/pkg/gh"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "gsn",
+		Short: "A small cli program to run my most usual tools and commands in my day to day as a Software Engineer",
+	}
+
+	// Define a command that accepts one argument
+	var showCmd = &cobra.Command{
+		Use:   "show [text]",
+		Short: "Print the provided text",
+		Run: func(cmd *cobra.Command, args []string) {
+			name, _ := cmd.Flags().GetString("name")
+			if name == "" {
+				fmt.Println("No name provided. Use -n or --name.")
+				return
+			}
+			fmt.Printf("Hello, %s!\n", name)
+		},
+	}
+
+	showCmd.Flags().StringP("name", "n", "", "Name to print")
+
+	rootCmd.AddCommand(showCmd)
+	rootCmd.AddCommand(gh.ApproveGhPrs())
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
---
+++ pkg/gh/gh.go
@@ -0,0 +1,48 @@
+package gh
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+func ApproveGhPrs() *cobra.Command {
+	var message string
+
+	approveCmd := &cobra.Command{
+		Use:   "approve <PR_URL>",
+		Short: "Approve a GitHub PR with optional message",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			prURL := args[0]
+
+			// Construct the gh command
+			var ghCmd *exec.Cmd
+			if message != "" {
+				ghCmd = exec.Command("gh", "pr", "review", prURL, "--approve", "--body", message)
+			} else {
+				ghCmd = exec.Command("gh", "pr", "review", prURL, "--approve")
+			}
+
+			// Attach stdout and stderr so you can see gh output
+			ghCmd.Stdout = os.Stdout
+			ghCmd.Stderr = os.Stderr
+
+			fmt.Println(prURL)
+			fmt.Println(message)
+
+			// Run the command
+			if err := ghCmd.Run(); err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to approve the PR: %v\n", err.Error())
+				os.Exit(1)
+			} else {
+				fmt.Println("Pull Request approved successfully!")
+			}
+		},
+	}
+
+	approveCmd.Flags().StringVarP(&message, "message", "m", "", "Optional review message")
+	return approveCmd
+}
